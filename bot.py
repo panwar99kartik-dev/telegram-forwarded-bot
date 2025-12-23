792744427ec47d65099a43917583975357b15bcd
panwar99kartik-dev <panwar99kartik@gmail.com>
2025-12-23 20:25:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ def list_groups(update, context):
         update.message.reply_text("I couldn't find any groups where I'm an admin.")
 
 def main():
-    updater = Updater("8403795351:AAFsCY7rLCWQbPsVXhuskHwxAtjgXxG4Q8o", use_context=True)
+    updater = Updater("8403795351:AAFsCY7rLCWQbPsVXhuskHwxAtjgXxG4Q8o")
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
